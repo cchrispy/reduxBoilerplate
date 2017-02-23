e4faa519366319915347cfaef8f9900ebe2a6e1a
Chris Chiang <cchiang1992@gmail.com>
2017-02-22 16:48:24 -0800

    Add script to start server using nodemon

--- server/server.js
+++ server/server.js
@@ -1,7 +1,11 @@
 var express = require('express');
+var path = require('path');
 var port = process.env.PORT || 8000;
 
 var app = express();
+
+app.use(express.static(path.resolve(__dirname, '..', 'dist')));
+
 
 app.listen(port);
 console.log(`Listening on port ${ port }`);
