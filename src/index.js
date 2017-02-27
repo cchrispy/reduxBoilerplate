edf3258dfc1bea60dadf9b7f79a3538d8c30ca8c
Chris Chiang <cchiang1992@gmail.com>
2017-02-27 15:00:44 -0800

    Install thunk middleware for async actions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 import { render } from 'react-dom';
 import mainReducer from './reducers/mainReducer';
 
 import App from './components/App.jsx';
 
-let store = createStore(mainReducer);
+let store = createStore(
+  mainReducer,
+  applyMiddleware(thunk)
+);
 
 const refresh = () => {
   render(
